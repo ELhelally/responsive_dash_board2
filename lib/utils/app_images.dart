c916ebfcefca0ba517e32386cc3c5fa7e133eb1a
ELhelally <ahmedsamehelhelallly@gmail.com>
2024-09-15 02:48:01 +0300

    generte assetes by flutter gen extention

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,92 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+
+  /// Assets for assetsImagesAvatr
+  /// lib/assets/images/avatr.svg
+  static const String assetsImagesAvatr = "lib/assets/images/avatr.svg";
+
+  /// Assets for assetsImagesAvatr2
+  /// lib/assets/images/avatr2.svg
+  static const String assetsImagesAvatr2 = "lib/assets/images/avatr2.svg";
+
+  /// Assets for assetsImagesBalnace
+  /// lib/assets/images/balnace.svg
+  static const String assetsImagesBalnace = "lib/assets/images/balnace.svg";
+
+  /// Assets for assetsImagesBeard3dRenderingIconIllustrationFreePng
+  /// lib/assets/images/beard-3d-rendering-icon-illustration-free-png.webp
+  static const String assetsImagesBeard3dRenderingIconIllustrationFreePng =
+      "lib/assets/images/beard-3d-rendering-icon-illustration-free-png.webp";
+
+  /// Assets for assetsImagesCategory2
+  /// lib/assets/images/category-2.svg
+  static const String assetsImagesCategory2 =
+      "lib/assets/images/category-2.svg";
+
+  /// Assets for assetsImagesChart2
+  /// lib/assets/images/chart-2.svg
+  static const String assetsImagesChart2 = "lib/assets/images/chart-2.svg";
+
+  /// Assets for assetsImagesConvertCard
+  /// lib/assets/images/convert-card.svg
+  static const String assetsImagesConvertCard =
+      "lib/assets/images/convert-card.svg";
+
+  /// Assets for assetsImagesExpenses
+  /// lib/assets/images/expenses.svg
+  static const String assetsImagesExpenses = "lib/assets/images/expenses.svg";
+
+  /// Assets for assetsImagesGallery
+  /// lib/assets/images/gallery.svg
+  static const String assetsImagesGallery = "lib/assets/images/gallery.svg";
+
+  /// Assets for assetsImagesGangsterFace3dRenderingIconIllustrationPng
+  /// lib/assets/images/gangster-face-3d-rendering-icon-illustration-png.webp
+  static const String assetsImagesGangsterFace3dRenderingIconIllustrationPng =
+      "lib/assets/images/gangster-face-3d-rendering-icon-illustration-png.webp";
+
+  /// Assets for assetsImagesIncome
+  /// lib/assets/images/income.svg
+  static const String assetsImagesIncome = "lib/assets/images/income.svg";
+
+  /// Assets for assetsImagesLogout
+  /// lib/assets/images/logout.svg
+  static const String assetsImagesLogout = "lib/assets/images/logout.svg";
+
+  /// Assets for assetsImagesMale3dAvatarFreePng
+  /// lib/assets/images/male-3d-avatar-free-png.webp
+  static const String assetsImagesMale3dAvatarFreePng =
+      "lib/assets/images/male-3d-avatar-free-png.webp";
+
+  /// Assets for assetsImagesMoneys
+  /// lib/assets/images/moneys.svg
+  static const String assetsImagesMoneys = "lib/assets/images/moneys.svg";
+
+  /// Assets for assetsImagesReadyPlayerMeAvatar
+  /// lib/assets/images/ReadyPlayerMe-Avatar.png
+  static const String assetsImagesReadyPlayerMeAvatar =
+      "lib/assets/images/ReadyPlayerMe-Avatar.png";
+
+  /// Assets for assetsImagesReadyPlayerMeAvatar (1)
+  /// lib/assets/images/ReadyPlayerMe-Avatar (1).png
+  static const String assetsImagesReadyPlayerMeAvatar1 =
+      "lib/assets/images/ReadyPlayerMe-Avatar (1).png";
+
+  /// Assets for assetsImagesSetting2
+  /// lib/assets/images/setting-2.svg
+  static const String assetsImagesSetting2 = "lib/assets/images/setting-2.svg";
+
+  /// Assets for assetsImagesStatistics
+  /// lib/assets/images/statistics.svg
+  static const String assetsImagesStatistics =
+      "lib/assets/images/statistics.svg";
+
+  /// Assets for assetsImagesVisa card
+  /// lib/assets/images/visa card.svg
+  static const String assetsImagesVisacard = "lib/assets/images/visa card.svg";
+
+  /// Assets for assetsImagesWallet2
+  /// lib/assets/images/wallet-2.svg
+  static const String assetsImagesWallet2 = "lib/assets/images/wallet-2.svg";
+}
