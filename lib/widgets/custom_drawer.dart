0aecda2e365104fcc7503721c3cfafe43403ae73
ELhelally <ahmedsamehelhelallly@gmail.com>
2024-09-15 05:19:39 +0300

    making a list of item model

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,23 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/darwer_item_model.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widgets/drawer_item.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_title.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-
+  static const List<DrawerItemModel> item = [
+    DrawerItemModel(title: 'Dashboard', image: Assets.assetsImagesCategory2),
+    DrawerItemModel(
+        title: 'My Transaction', image: Assets.assetsImagesConvertCard),
+    DrawerItemModel(title: 'Statistics', image: Assets.assetsImagesStatistics),
+    DrawerItemModel(title: 'Wallet Account', image: Assets.assetsImagesWallet2),
+    DrawerItemModel(title: 'My Investments', image: Assets.assetsImagesChart2),
+  ];
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
         color: Colors.white,
       ),
-      child: const Column(
+      child: Column(
         children: [
-          UserInfoListTitle(
+          const UserInfoListTitle(
               image:
                   Assets.assetsImagesGangsterFace3dRenderingIconIllustrationPng,
               title: 'Lekan Okeowo',
-              subTitle: 'demo@gmail.com')
+              subTitle: 'demo@gmail.com'),
+          const SizedBox(
+            height: 8,
+          ),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: item.length,
+            itemBuilder: (context, index) {
+              return Padding(
+                padding: const EdgeInsets.only(top: 20),
+                child: DrawerItem(darwerItemModel: item[index]),
+              );
+            },
+          )
         ],
       ),
     );
