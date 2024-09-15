460e4c10bb7cc0f2c2f0bc6afc7c05f726d30616
ELhelally <ahmedsamehelhelallly@gmail.com>
2024-09-15 13:29:57 +0300

    add list of item to the drawerItemListViewBulider to refactor the code

--- lib/widgets/custom_drawer.dart
+++ lib/widgets/custom_drawer.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/models/darwer_item_model.dart';
+
 import 'package:responsive_dash_board/utils/app_images.dart';
-import 'package:responsive_dash_board/widgets/drawer_item.dart';
+
 import 'package:responsive_dash_board/widgets/drawer_item_list_view_bulider.dart';
 import 'package:responsive_dash_board/widgets/user_info_list_title.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
-  static const List<DrawerItemModel> item = [
-    DrawerItemModel(title: 'Dashboard', image: Assets.assetsImagesCategory2),
-    DrawerItemModel(
-        title: 'My Transaction', image: Assets.assetsImagesConvertCard),
-    DrawerItemModel(title: 'Statistics', image: Assets.assetsImagesStatistics),
-    DrawerItemModel(title: 'Wallet Account', image: Assets.assetsImagesWallet2),
-    DrawerItemModel(title: 'My Investments', image: Assets.assetsImagesChart2),
-  ];
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -31,7 +23,7 @@ class CustomDrawer extends StatelessWidget {
           SizedBox(
             height: 8,
           ),
-          DrawerItemListViewBulider(item: item)
+          DrawerItemListViewBulider()
         ],
       ),
     );
--- lib/widgets/drawer_item_list_view_bulider.dart
+++ lib/widgets/drawer_item_list_view_bulider.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/models/darwer_item_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widgets/drawer_item.dart';
 
 class DrawerItemListViewBulider extends StatelessWidget {
   const DrawerItemListViewBulider({
     super.key,
-    required this.item,
   });
 
-  final List<DrawerItemModel> item;
+  // final List<DrawerItemModel> item;
 
+  static const List<DrawerItemModel> item = [
+    DrawerItemModel(title: 'Dashboard', image: Assets.assetsImagesCategory2),
+    DrawerItemModel(
+        title: 'My Transaction', image: Assets.assetsImagesConvertCard),
+    DrawerItemModel(title: 'Statistics', image: Assets.assetsImagesStatistics),
+    DrawerItemModel(title: 'Wallet Account', image: Assets.assetsImagesWallet2),
+    DrawerItemModel(title: 'My Investments', image: Assets.assetsImagesChart2),
+  ];
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
